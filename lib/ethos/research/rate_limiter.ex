2bef6217446dcff290db57da06530e7511292d5c
Joe Stein <cryptcom@gmail.com>
2026-08-17 11:09:44 -0400

    fix: serialize rate limiter checks through GenServer

--- lib/ethos/research/rate_limiter.ex
+++ lib/ethos/research/rate_limiter.ex
@@ -1,5 +1,5 @@
 defmodule Ethos.Research.RateLimiter do
-  @moduledoc "ETS sliding-window limiter: 10 research calls per user per hour. Resets on deploy (accepted per spec)."
+  @moduledoc "ETS sliding-window limiter: 10 research calls per user per hour. Resets on deploy (accepted per spec). All checks serialized through GenServer to avoid race conditions."
   use GenServer
 
   @table __MODULE__
@@ -9,6 +9,21 @@ defmodule Ethos.Research.RateLimiter do
   def start_link(_), do: GenServer.start_link(__MODULE__, nil, name: __MODULE__)
 
   def allow?(user_id) do
+    GenServer.call(__MODULE__, {:allow?, user_id})
+  end
+
+  def reset(user_id) do
+    GenServer.call(__MODULE__, {:reset, user_id})
+  end
+
+  @impl true
+  def init(nil) do
+    :ets.new(@table, [:named_table, :protected, :set])
+    {:ok, nil}
+  end
+
+  @impl true
+  def handle_call({:allow?, user_id}, _from, state) do
     now = System.monotonic_time(:millisecond)
     cutoff = now - @window_ms
     stamps = case :ets.lookup(@table, user_id) do
@@ -16,20 +31,13 @@ defmodule Ethos.Research.RateLimiter do
       [] -> []
     end
 
-    if length(stamps) < @limit do
-      :ets.insert(@table, {user_id, [now | stamps]})
-      true
-    else
-      :ets.insert(@table, {user_id, stamps})
-      false
-    end
+    allowed = length(stamps) < @limit
+    :ets.insert(@table, {user_id, if(allowed, do: [now | stamps], else: stamps)})
+    {:reply, allowed, state}
   end
 
-  def reset(user_id), do: :ets.delete(@table, user_id)
-
-  @impl true
-  def init(nil) do
-    :ets.new(@table, [:named_table, :public, :set])
-    {:ok, nil}
+  def handle_call({:reset, user_id}, _from, state) do
+    :ets.delete(@table, user_id)
+    {:reply, :ok, state}
   end
 end
